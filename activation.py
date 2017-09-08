fe5151629f39c95da710145dce3713eaa7c5b240
beekbin <songbinliu@hotmail.com>
2017-09-07 23:40:07 -0400

    Create activation.py

---
+++ activation.py
@@ -0,0 +1,41 @@
+from __future__ import division
+from __future__ import print_function
+import numpy as np
+
+
+class ActiveFunction(object):
+    def __init__(self, name):
+        self.name = name
+
+    def forward(self, x):
+        pass
+
+    def backward(self, y):
+        pass
+
+    def __str__(self):
+        return self.name
+
+
+class SigmoidActiveFunction(ActiveFunction):
+    def __init__(self):
+        super(self.__class__, self).__init__("sigmoid")
+        return
+
+    def forward(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def backward(self, y):
+        return y * (1-y)
+
+
+class TanhActiveFunction(ActiveFunction):
+    def __init__(self):
+        super(self.__class__, self).__init__("tanh")
+        return
+
+    def forward(self, x):
+        return np.tanh(x)
+
+    def backward(self, y):
+        return 1 - y*y
